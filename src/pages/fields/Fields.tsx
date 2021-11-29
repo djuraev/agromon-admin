b3702ded62866ec62056be6610e949e9a5df8172
Djuraev Mamurjon <mamurjon@nextree.io>
2021-11-29 21:03:21 +0500

    New layout

--- src/pages/fields/Fields.tsx
+++ src/pages/fields/Fields.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import Map from './Map';
+import ReactMapboxGl from "react-mapbox-gl";
+import DrawControl from "react-mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 
 interface Props {
 
@@ -8,12 +10,40 @@ interface Props {
 interface State {
 
 }
+
+const Map = ReactMapboxGl({
+    accessToken:
+        "pk.eyJ1IjoiZGFpbWsiLCJhIjoiY2ttbmt2dzc2MXZ1bjJwcGZsZndoaGdkbiJ9.KzEXKpaGb0yYkV8Npdg65g"
+});
+
 class Fields extends Component<Props, State> {
     //
+    onDrawCreate(points: any){
+        //console.log(features);
+        const {features} = points;
+        console.log(features[0].geometry.coordinates);
+
+    };
+
+    onDrawUpdate(points: any ){
+        const {features} = points;
+        console.log(features[0].geometry.coordinates);
+    };
 
     render() {
         return (
-            <Map />
+            <div>
+                <h2>Draw Polygon</h2>
+                <Map
+                    style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
+                    containerStyle={{
+                        height: "600px",
+                        width: "400px"
+                    }}
+                >
+                    <DrawControl onDrawCreate={this.onDrawCreate} onDrawUpdate={this.onDrawUpdate} />
+                </Map>
+            </div>
         );
     }
 }
